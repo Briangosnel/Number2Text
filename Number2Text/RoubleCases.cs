cab3a9e07fc7a163239d678f8c3a95d6ea29d881
Briangosnel <109523312+Briangosnel@users.noreply.github.com>
2022-07-22 12:56:25 +0300

    Добавление склонения рублей

---
+++ Number2Text/RoubleCases.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Number2Text
+{
+    public class RoubleCases
+    {
+        public string ConvertPadej(int value)
+        {
+            int div = value % 10;
+            int div10 = (value / 10) % 10;
+
+            if (div10 == 1) return "рублей";
+
+            if (div == 0 || div >= 5 && div <= 9) return "рублей";
+            if (div == 1) return "рубль";
+            if (div >= 2 && div <= 4 && div10 != 1) return "рубля";
+
+            return "";
+        }
+    }
+}
--- Number2TextTest2/UnitTest1.cs
+++ Number2TextTest2/UnitTest1.cs
@@ -31,6 +31,27 @@ namespace Number2TextTest2
             Object obj = new Number2Text.Number2Text().Convert(10000);
             Assert.IsInstanceOfType(obj, typeof(string));
         }
+        [TestMethod]
+        public void TestMethodConvert6()
+        {
+            Assert.AreEqual("рубль", new Number2Text.RoubleCases().ConvertPadej(1));
+        }
+        [TestMethod]
+        public void TestMethodConvert7()
+        {
+            Assert.AreEqual("рубля", new Number2Text.RoubleCases().ConvertPadej(102));
+        }
+        [TestMethod]
+        public void TestMethodConvert8()
+        {
+            Assert.AreEqual("рублей", new Number2Text.RoubleCases().ConvertPadej(11));
+        }
+        [TestMethod]
+        public void TestMethodConvert9()
+        {
+            Assert.AreEqual("рублей", new Number2Text.RoubleCases().ConvertPadej(1011));
+        }
+
 
     }
 }
